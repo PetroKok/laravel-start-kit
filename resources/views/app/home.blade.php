9db0afa6c5603e0402e0feac375e6297e5f5525e
Petro <kokpetro15@gmail.com>
2019-05-15 00:05:55 +0300

    changed env

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,20 +1,17 @@
 @extends('app.layouts.app')
 
 @push("styles")
-    <link rel="stylesheet" href="{{asset('css/profile/style4.css')}}">
-    <link rel="stylesheet" href="{{asset('css/react-notify.css')}}">
+    <link rel="stylesheet" href="/css/profile/style4.css">
+    <link rel="stylesheet" href="/css/react-notify.css">
 
-<!-- Our Custom CSS -->
-{{--<link rel="stylesheet" href="style4.css">--}}
-
-<!-- Font Awesome JS -->
-<script type="text/javascript" src="{{asset('js/solid.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/fontawesome.js')}}"></script>
+    <!-- Font Awesome JS -->
+    <script type="text/javascript" src="/js/solid.js"></script>
+    <script type="text/javascript" src="/js/fontawesome.js"></script>
 
 @endpush
 
 @push('scripts')
-    <script type="text/javascript" src="{{asset('js/pages/profile.js')}}"></script>
+    <script type="text/javascript" src="/js/pages/profile.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function () {
